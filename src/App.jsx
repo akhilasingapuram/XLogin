847bbac4a877fc7f331a28caf24327421f4fabd3
akhilasingapuram <singapuram.akhila@gmail.com>
2025-02-23 21:31:28 +0530

    updated the code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === "user" && password === "password") {
+      setIsLoggedIn(true);
+      setIsError(false);
+    } else {
+      setIsError(true);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {isError && <p>Invalid username or password</p>}
+      {isLoggedIn ? (
+        <p>Welcome, {username}!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              value={username}
+              required={username === ""}
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              required={password === ""}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
+};
+export default Login;
